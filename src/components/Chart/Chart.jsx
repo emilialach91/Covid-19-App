51c5a8d0ae024bad41b01c3a3328919ae9c3549f
Emilia <emilia.lach.91@gmail.com>
2021-02-13 21:42:32 +0000

    chart basic config

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,32 +5,30 @@ import styles from './Chart.module.css';
 
 const Chart = () => {
 
-    const [dailyData, setDailyData] = useState({ });
+    const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => { 
             setDailyData(await fetchDailyData());
         }
 
-        console.log(dailyData);
-
         fetchAPI();
     });
 
     const lineChart = (
-        dailyData[0]
+        dailyData.length 
         ? (
-            <line 
+            <Line 
             data={{
-                labels: dailyData(({ date }) => date),
+                labels: dailyData.map(({ date }) => date),
                 datasets:[{
-                    data: dailyData(({ confirmed }) => confirmed),
+                    data: dailyData.map(({ confirmed }) => confirmed),
                     label:'infected',
                     borderColor:'#3333ff',
                     fill: true,
                 }, 
                 {
-                    data: dailyData(({ deaths }) => deaths),
+                    data: dailyData.map(({ deaths }) => deaths),
                     label: 'deaths',
                     borderColor:'red',
                     backgroundColor: 'rgb(255, 0, 0, 0.6)',
